a1e5cede895b1514c7c095d12abe028e770b766e
dmithamo <dennis.mithamo@sportserve.co>
2024-07-30 18:59:53 +0300

    figure out custom fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <div className={'h-screen flex flex-col justify-center items-center'}>
       <h1 className="text-3xl font-bold">Hello world!</h1>
       <p>Welcome to the re-write</p>
+      <img src={'/bims-logo.svg'} alt="Bims logo" width={150} height={150} />
     </div>
   );
 }
